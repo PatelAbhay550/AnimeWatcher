8ec75239d8dedff6427f09de038a85de9379adf6
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-01-20 23:31:42 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="fixed top-0 left-0  backdrop-blur-sm gap-4 text-3xl bg-white/30 w-full h-12 z-40 flex items-center justify-center">
-      <Image src="/logo.svg" width={40} height={40} alt="logo" /> Anime Watcher
+      <Image src="/logo.svg" width={40} height={40} alt="logo" /> <h1>Anime Watcher</h1>
     </div>
   );
 };
