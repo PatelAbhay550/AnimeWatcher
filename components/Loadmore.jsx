902474779fae5b2cc8229fb1f13be94d9865455a
Abhay Patel <patelabhay550@gmail.com>
2024-01-20 23:19:27 +0530

    first commit

---
+++ components/Loadmore.jsx
@@ -0,0 +1,79 @@
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { ImSpinner } from "react-icons/im";
+import { fetchAnime } from "@/app/action";
+import { MotionDiv } from "./MotionDiv";
+let page = 2;
+const Loadmore = () => {
+  const { ref, inView } = useInView();
+  const [animeData, setAnimeData] = useState([]);
+  const variants = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1 },
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      if (inView) {
+        try {
+          const newData = await fetchAnime(page);
+          setAnimeData((prevData) => [...prevData, ...newData]);
+          page++;
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }
+    };
+
+    fetchData();
+  }, [inView]);
+
+  return (
+    <>
+      <div className="flex min-h-screen flex-col md:flex-row md:gap-5 gap-2 ">
+        {animeData.map((item) => (
+          <MotionDiv
+            initial="hidden"
+            animate="visible"
+            transition={{
+              delay: 0.5,
+              ease: "easeInOut",
+              duration: 0.5,
+            }}
+            variants={variants}
+            key={item.id}
+            className="text-3xl mb-4 h-80 w-80 overflow-hidden rounded-lg backdrop-blur-sm bg-white/30"
+          >
+            <h1 className="text-slate-900 flex items-center justify-center font-sans font-bold bg-lime-300 h-1/5">
+              {item.name}
+            </h1>
+            <div className="dets w-full flex h-4/5">
+              <Image
+                className="h-full object-cover w-2/3"
+                src={`https://shikimori.one${item.image.original}`}
+                alt={item.name}
+                width={100}
+                height={200}
+              />
+              <div className="data text-white text-lg pl-3 flex flex-col gap-1">
+                <h2>Name: {item.name}</h2>
+                <h2>Kind: {item.kind}</h2>
+                <h2>Score: {item.score}</h2>
+                <h2>Released: {item.released_on}</h2>
+              </div>
+            </div>
+          </MotionDiv>
+        ))}
+      </div>
+
+      <div className="main w-full flex items-center justify-center" ref={ref}>
+        {inView && (
+          <ImSpinner className="animate-spin text-4xl text-lime-500 mt-5" />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Loadmore;
---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 left-0  backdrop-blur-sm gap-4 text-3xl bg-white/30 w-full h-12 z-40 flex items-center justify-center">
+      <Image src="/logo.svg" width={40} height={40} alt="logo" /> Anime Watcher
+    </div>
+  );
+};
+
+export default Navbar;
